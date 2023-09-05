79e9083bc5811f1969b603bf640618f0894a9cba
rahulkallingal <106032492+rahulkallingal@users.noreply.github.com>
2023-09-05 07:09:52 +0530

    Delete Pass directory

--- Pass/passwordmanager/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . models import UserRegistration
-
-class UserRegistrationForm(forms.ModelForm):
-    class Meta:
-        app_label = 'passwordmanager'
-        model = UserRegistration
-        fields = ['username','password']
