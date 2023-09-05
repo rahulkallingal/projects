5bff122222a899b4f135c9fea5a13bb26999a377
rahulkallingal <106032492+rahulkallingal@users.noreply.github.com>
2023-09-05 20:59:17 +0530

    Delete PasswordManager directory

--- PasswordManager/passwordmanager/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class UserRegistration(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=100)
