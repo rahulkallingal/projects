dd7225c896a8397a460b2926558ee72d0a025af8
rahulkallingal <106032492+rahulkallingal@users.noreply.github.com>
2023-09-12 20:06:18 +0530

    all pages are integrated

--- DisasterManagement/HelpingHand/DisasterManagementApp/urls.py
+++ DisasterManagement/HelpingHand/DisasterManagementApp/urls.py
@@ -19,5 +19,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('index', views.index, name = 'index'),
+    path('/about', views.about, name = 'about'),
+    path('/contact', views.contact, name = 'contact'),
+    path('/guard', views.guard, name = 'guard'),
+    path('/service', views.service, name = 'service')
 ]
--- DisasterManagement/HelpingHand/DisasterManagementApp/views.py
+++ DisasterManagement/HelpingHand/DisasterManagementApp/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def guard(request):
+    return render(request, 'guard.html')
+
+
+def service(request):
+    return render(request, 'service.html')
